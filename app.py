fc720d218159a904086387f0aff7dc62c101cd29
Nimrod <nimrod@wear-2b.com>
2022-06-20 13:05:55 +0300

    added prints

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from db import db
 
 app = Flask(__name__)
 app.config['PROPAGATE_EXCEPTIONS'] = True  # To allow flask propagating exception even if debug is set to false on app
+print(os.environ.get('DATABASE_URL_SQL'))
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL_SQL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'nimrod'
